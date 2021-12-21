683fd5070b3884f693be61634dfb03028473a20b
ferxanni <91278641+ferxanni@users.noreply.github.com>
2021-12-21 21:55:59 +0200

    Update Funkcijos.h

--- Funkcijos.h
+++ Funkcijos.h
@@ -1,57 +1,88 @@
-#ifndef FUNCTIONS_H
-#define FUNCTIONS_H
+#ifndef FUNKCIJOS_H
+#define FUNKCIJOS_H
 #include "libraries.h"
-
-
-#include <iostream>
-#include <vector>
-#include <algorithm>
 
 class data
 {
 private:
-    std::string name;
-    std::string surn;
-    std::vector <int> marks;
+    std::string Name;
+    std::string Surname;
     int exam;
-    double vidurkis;
-    double mediana;
-    bool CorrectData;
+    vector <int> marks;
+    bool CorrectData = true;
+    double Total = 0;
+    double Vidurkis;
+    double Mediana;
+    int StudSk;
+
 
 public:
     data();
 
-    void setname(std::string x);
-    std::string getname()const;
+    void setName(std::string x);
+    std::string getName()const;
 
-    void setsurn(std::string x);
-    std::string getsurn()const;
+    void setSurname(std::string x);
+    std::string getSurname()const;
 
     void set_exam(int exam_mark);
     int get_exam()const;
 
+    void set_StudSk(int sk);
+    int get_StudSk()const;
+
     void set_mark(double mark);
     int get_mark(int a);
-    void ClearMark();
+    void Clearmarks();
 
-    double getvidurkis()const;
-    double getmediana()const;
+    double getVidurkis()const;
+    double getMediana()const;
 
-    double Skaiciuojam_vidurkis();
-    double Skaiciuojam_mediana();
+    double Skaiciuojam_Vidurki();
+    double Skaiciuojam_Mediana();
 
     bool setCorrectData(bool x);
     bool getCorrectData();
 
+
+    friend bool operator > (const data& a, const data& b);
+    friend bool operator < (const data& a, const data& b);
+    friend bool operator >= (const data& a, const data& b);
+    friend bool operator <= (const data& a, const data& b);
+    friend bool operator == (const data& a, const data& b);
+    friend bool operator != (const data& a, const data& b);
+
+    friend std::istream& operator >> (std::istream& in, data& a);
+    friend std::ostream& operator << (std::ostream& out, data& a);
+
 };
+
+
+
+bool Skirstom(const data& a);
+void Studentu_grupes(list <data>& sze);
+void  Skaitom_faila(list <data>& sze);
+void Generuojam_Sarasa();
+
+
 
 bool Lyginam_pagal_rezultatus(const data& a, const data& b);
 bool Lyginam_pagal_varda(const data& a, const data& b);
-bool Skirstom(data& a);
-void Studentu_grupes(list <data>& sze);
-void Skaitom_faila(list <data>& sze);
-void Generuojam_sarasa(int StudSK);
-// int GetLongestString(list <student>& A);
+
+int GetLongestString(list <data>& sze);
+unsigned int wordsinstring(std::string const& str);
+
+
+
+
+int randomMark();
+
+void Input(list <data>& sze);
+void Output(list <data>& sze);
+
+void isprintinam_data(list <data> sze);
+void Ivedimas(list <data>& sze);
+
 
 
 #endif
